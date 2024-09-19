0fd784e663d4c31287d1a3722a517a3b941c6d88
dacaiguoguo <dacaiguoguo@163.com>
2024-09-19 20:27:56 +0800

    Initial Commit

---
+++ HapticDemo/HapticDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticDemoApp.swift
+//  HapticDemo
+//
+//  Created by yanguo sun on 2024/9/19.
+//
+
+import SwiftUI
+
+@main
+struct HapticDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
